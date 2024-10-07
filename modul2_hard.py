b19e1b45716604c1bac9080eaaadb66c9f42b56d
Berg045 <perov.ka@gmail.com>
2024-10-07 17:02:40 +0300

    Update modul2_hard.py

--- modul2_hard.py
+++ modul2_hard.py
@@ -6,14 +6,14 @@ result = []
 for i in range(1, 21):
     i_pairs = []
     for j in range(1, 21):
-        if j != n and (i + j) % n == 0:
+        if j != n and n % (i + j) == 0:
             i_pairs.append(j)
 
     for pair in i_pairs:
         if pair not in result:
             result.append(i)
             result.append(pair)
-            break
+        break
 
 print(n, ":",* result)
 
