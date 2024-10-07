fb5ff68f3dd4bc3a66b8f9a543e4b4120b220136
Berg045 <perov.ka@gmail.com>
2024-10-08 00:16:41 +0300

    Update modul2_hard.py

--- modul2_hard.py
+++ modul2_hard.py
@@ -1,19 +1,20 @@
 import random
 
+# Начальное число и список для хранения результата
 n = random.randint(3, 20)
 result = []
 
 for i in range(1, 21):
     i_pairs = []
+
     for j in range(1, 21):
-        if j != n and n % (i + j) == 0:
+        if j != i and n % (i + j) == 0:
             i_pairs.append(j)
 
     for pair in i_pairs:
         if pair not in result:
             result.append(i)
             result.append(pair)
-        break
 
-print(n, ":",* result)
+print( n, ":", * result)
 
