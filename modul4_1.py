04b05d0f82e6d43c9292c82c7a94e85a47e8f5c9
Berg045 <perov.ka@gmail.com>
2024-10-21 18:27:33 +0300

    Add files via upload

---
+++ modul4_1.py
@@ -0,0 +1,16 @@
+import fake_math as fm
+import true_math as tm
+
+# Импорт функций из модулей fake_math и true_math
+fm_divide = fm.divide
+tm_divide = tm.divide
+
+result1 = fm_divide(69, 3)
+result2 = fm_divide(3, 0)
+result3 = tm_divide(49, 7)
+result4 = tm_divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
