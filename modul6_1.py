b1872b90215c67fbb23f537d07e3e4e221320553
Berg045 <perov.ka@gmail.com>
2024-11-11 13:35:47 +0300

    Update modul6_1.py

--- modul6_1.py
+++ modul6_1.py
@@ -73,22 +73,22 @@ class Flower(Plant):
 class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
-        self.edible = True  # Переопределяем атрибут edible
+        self.edible = True 
 
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
-# Проверка атрибутов и выполнение методов
-print(a1.name)  # Волк с Уолл-Стрит
-print(p1.name)  # Цветик семицветик
 
-print(a1.alive)  # True
-print(a2.fed)    # False
+print(a1.name)  
+print(p1.name) 
 
-a1.eat(p1)  # Волк с Уолл-Стрит не стал есть Цветик семицветик
-a2.eat(p2)  # Хатико съел Заводной апельсин
+print(a1.alive) 
+print(a2.fed)  
 
-print(a1.alive)  # False
-print(a2.fed)    # True
+a1.eat(p1) 
+a2.eat(p2)  
+
+print(a1.alive) 
+print(a2.fed) 
