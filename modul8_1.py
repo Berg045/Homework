ea56156962acb1ea6f25275572cc839d984399ba
Berg045 <perov.ka@gmail.com>
2024-11-28 19:08:20 +0300

    Update modul8_1.py

--- modul8_1.py
+++ modul8_1.py
@@ -6,7 +6,6 @@ def add_everything_up(a, b):
     else:
         return str(a) + str(b)
 
-# Примеры использования функции
-print(add_everything_up(123.456, 'строка'))  # 123.456строка
-print(add_everything_up('яблоко', 4215))    # яблоко4215
-print(add_everything_up(123.456, 7))       # 130.456
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215)) 
+print(add_everything_up(123.456, 7)) 
